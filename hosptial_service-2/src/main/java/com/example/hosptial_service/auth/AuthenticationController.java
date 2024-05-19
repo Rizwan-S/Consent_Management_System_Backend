e921bc617f3a1a8ca86fe44c65cdd9bf5677e76a
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2024-05-19 15:13:34 +0530

    Implemented backend for consent management system.

---
+++ hosptial_service-2/src/main/java/com/example/hosptial_service/auth/AuthenticationController.java
@@ -0,0 +1,22 @@
+package com.example.hosptial_service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationService service;
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+      @RequestBody AuthenticationRequest request
+  ) {
+    return ResponseEntity.ok(service.authenticate(request));
+  }
+
+
+}
